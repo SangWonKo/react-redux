93b4031108d0f27bedb85529d83c2246bbf4e928
SangWonKo <swko1017@naver.com>
2022-09-12 18:11:40 +0900

    카운터 앱 세팅 및 미들웨어 비동기 처리 준비

--- create-redux/app.js
+++ create-redux/app.js
@@ -1,16 +1,35 @@
-import { createStore } from "./redux.js";
-import { reducer } from "./reducer.js";
-import * as Actions from "./actions.js";
+import { createStore } from './redux.js';
+import * as Actions from './actions.js';
+import reducer from './reducer.js';
 
 const store = createStore(reducer);
 
-store.subscribe(function () {
-  console.log(store.getState());
+const counterDisplay = document.querySelector('#counter');
+const btnIncrease = document.querySelector('#btn-increase');
+const btnAsyncIncrease = document.querySelector('#btn-async-increase');
+const btnDecrease = document.querySelector('#btn-decrease');
+const btnReset = document.querySelector('#btn-reset');
+
+store.subscribe(function() {
+  const { counter } = store.getState();
+
+  counterDisplay.textContent = counter;
 });
 
-store.dispatch(Actions.increase());
-store.dispatch(Actions.increase());
-store.dispatch(Actions.increase());
-store.dispatch(Actions.decrease());
-store.dispatch(Actions.reset());
+store.dispatch(Actions.setCounter(0));
 
+btnReset.addEventListener('click', () => {
+  store.dispatch(Actions.setCounter(0));
+});
+
+btnIncrease.addEventListener('click', () => {
+  store.dispatch(Actions.increase());
+});
+
+btnAsyncIncrease.addEventListener('click', () => {
+  store.dispatch(Actions.asyncIncrease({ url: '/async-increase' }));
+});
+
+btnDecrease.addEventListener('click', () => {
+  store.dispatch(Actions.decrease());
+});
--- create-redux/reducer.js
+++ create-redux/reducer.js
@@ -1,18 +1,38 @@
-import * as ActionType from "./action_type.js";
+import * as Actions from "./action_type.js";
 
-const InitializeState = { count: 0 };
+const InitializeState = {
+  message: "app store",
+};
+
 //상태를 변경하는 메소드
 //state deep copy -> 항상 새로운 상태를 반환해야함 (참조 무결성이 깨짐)
 // spread 연산자 사용 -> two depth 이상의 상태 디자인을 지양함
-export function reducer(state = InitializeState, action) {
+export default function reducer(state = InitializeState, action) {
   switch (action.type) {
-    case ActionType.INCREASE:
-      return { ...state, count: state.count + 1 };
-    case ActionType.DECREASE:
-      return { ...state, count: state.count - 1 };
-    case ActionType.RESET:
-      return { ...state, count: 0 };
+    case Actions.INCREASE_COUNTER:
+      return {
+        ...state,
+        counter: state.counter === undefined ? 1 : state.counter + 1,
+      };
+    case Actions.ASYNC_INCREASE_COUNTER:
+      fetch(action.payload.url)
+        .then((response) => response.json())
+        .then((result) => {
+          return { ...state };
+        })
+        .catch((err) => {
+          return { ...state };
+        });
+    case Actions.DECREASE_COUNTER:
+      return {
+        ...state,
+        counter: state.counter === undefined ? 0 : state.counter - 1,
+      };
+    case Actions.SET_COUNTER:
+      return { ...state, counter: action.payload };
     default:
       return { ...state };
   }
 }
+
+//reducer는 순수함수를 지향, 외부 의존성 x -> 비동기 처리는 reducer 외부에서
