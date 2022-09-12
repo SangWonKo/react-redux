9fe522a602390e4374d4965144f729d4f0f3efda
SangWonKo <swko1017@naver.com>
2022-09-12 19:25:44 +0900

    비동기 미들웨어 구현 및 loading 상태 추가

--- create-redux/app.js
+++ create-redux/app.js
@@ -4,6 +4,26 @@ import reducer from "./reducer.js";
 import { ASYNC_INCREASE_COUNTER, SET_COUNTER } from "./action_type.js";
 import { logger } from "./logger.js";
 
+const asyncRouter = (jobs) => (store) => (next) => (action) => {
+  const matchJob = Object.entries(jobs).find(([type]) => action.type === type);
+
+  if(matchJob) {
+    matchJob[1](store, action);
+  } else {
+    next(action);
+  }
+};
+
+const asyncJobs = {
+  [ASYNC_INCREASE_COUNTER]: function (store, action) {
+    store.dispatch(Actions.asyncRequest());
+    setTimeout(() => {
+      store.dispatch(Actions.increase(20));
+      store.dispatch(Actions.asyncResponse());
+    }, 3000);
+  },
+};
+
 // const middleware1 = (store) => (next) => (action) => {
 //   console.log("m1", action);
 //   next(action); //앞에 있는 dispatch에 action 전달
@@ -29,17 +49,19 @@ import { logger } from "./logger.js";
 //   // dispatch(action); //앞에 있는 dispatch에 action 전달
 // };
 
-const store = createStore(reducer, [logger]);
+const store = createStore(reducer, [logger, asyncRouter(asyncJobs)]);
 
 const counterDisplay = document.querySelector("#counter");
+const loadingMessage = document.querySelector("#loading");
 const btnIncrease = document.querySelector("#btn-increase");
 const btnAsyncIncrease = document.querySelector("#btn-async-increase");
 const btnDecrease = document.querySelector("#btn-decrease");
 const btnReset = document.querySelector("#btn-reset");
 
 store.subscribe(function () {
-  const { counter } = store.getState();
+  const { counter, request } = store.getState();
 
+  loadingMessage.style.visibility = request ? "visible" : "hidden";
   counterDisplay.textContent = counter;
 });
 
